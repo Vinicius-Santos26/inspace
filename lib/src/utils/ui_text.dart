52431b1184a896593977fb606a4e5e67c2d9261b
Giovanna Benedetti Ferreira <62912397+gigibenedetti@users.noreply.github.com>
2022-06-10 20:46:33 -0300

    Global Solution - parte Giovanna
    Adição das Strings.

--- lib/src/utils/ui_text.dart
+++ lib/src/utils/ui_text.dart
@@ -1,1 +1,20 @@
-
+const String login = 'Login';
+const String eMail = 'E-mail';
+const String password = 'Senha';
+const String home = 'Home';
+const String newReservation = 'Nova reserva';
+const String myReservations = 'Minhas reservas';
+const String cancelReservation = 'Cancelar reservas';
+const String members = 'Integrantes';
+const String popUpTitle = 'Integrantes - Turma 3SIR';
+const String carolInfo = 'Carolina de Jesus Bonametti RM:85513';
+const String giovannaInfo = 'Giovanna Benedetti Ferreira RM:85267';
+const String viniciusInfo = 'Vinicius de Oliveira Santos RM:86082';
+const String settings = 'Configurações';
+const String darkMode = 'Dark Mode';
+const String selectDate = 'Selecione uma data para a viagem';
+const String select = 'Selecionar';
+const String amountOfPeople = 'Quantidade de pessoas';
+const String reserve = 'Reservar';
+const String checkIn = 'Realizar Check-in';
+const String cancel = 'Cancelar';
