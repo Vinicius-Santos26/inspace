cc53e7bd81a7d07a5c4d74796c93e710c7babf80
carolinabonametti <60740114+carolinabonametti@users.noreply.github.com>
2022-06-10 21:13:21 -0300

    Global Solution - Parte Carol

--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 
+const _primaryColor = Colors.blue;
+const _secondaryColor = Colors.green;
+const _iconColor = Colors.white;
+const _textColor = Colors.white;
+
+ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: _primaryColor,
+    appBarTheme: const AppBarTheme(
+      titleTextStyle: TextStyle(color: _textColor, fontSize: 24),
+      backgroundColor: _primaryColor,
+      iconTheme: IconThemeData(color: _iconColor),
+    ));
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primarySwatch: _secondaryColor,
+  appBarTheme: const AppBarTheme(
+      titleTextStyle: TextStyle(color: _textColor, fontSize: 24),
+      backgroundColor: _secondaryColor,
+      iconTheme: IconThemeData(color: _iconColor)),
+);
